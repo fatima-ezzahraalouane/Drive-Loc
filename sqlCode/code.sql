8afed009474a10c1b141a3a42fc9463dd23b8a20
fatima-ezzahraalouane <falouane38@gmail.com>
2024-12-31 09:55:14 +0100

    #4 creer la table role gere les roles admin ou client

--- sqlCode/code.sql
+++ sqlCode/code.sql
@@ -2,3 +2,9 @@ CREATE DATABASE DriveLoc;
 
 USE DriveLoc;
 
+-- Table Role
+CREATE TABLE role (
+    id_role int NOT null AUTO_INCREMENT PRIMARY KEY,
+    name_user ENUM('Admin', 'Client') NOT NULL
+);
+
