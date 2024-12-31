92f09c7ad4018302fdf7d5f69afe56f148a04a19
fatima-ezzahraalouane <falouane38@gmail.com>
2024-12-31 10:03:33 +0100

    #5 creer la table user site Afin de gérer les informations des utilisateurs et leurs rôles associés.

--- sqlCode/code.sql
+++ sqlCode/code.sql
@@ -8,3 +8,13 @@ CREATE TABLE role (
     name_user ENUM('Admin', 'Client') NOT NULL
 );
 
+-- Table UserSite
+CREATE TABLE usersite (
+    id_user int NOT null AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(250) NOT NULL UNIQUE,
+    email VARCHAR(250) NOT NULL UNIQUE,
+    telephone varchar(50) NOT null UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    id_role int NOT null,
+    FOREIGN KEY (id_role) REFERENCES role(id_role)
+);
