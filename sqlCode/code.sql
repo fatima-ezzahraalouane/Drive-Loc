ecfbcf0ad10fdd647c0284e911f2e4b334bc2b38
fatima-ezzahraalouane <falouane38@gmail.com>
2024-12-31 11:06:03 +0100

    #6 creer la table vehicule Afin de stocker les informations relatives aux véhicules.

--- sqlCode/code.sql
+++ sqlCode/code.sql
@@ -22,5 +22,21 @@ CREATE TABLE usersite (
 CREATE TABLE categorie (
     id_categorie int NOT null AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(250) NOT NULL UNIQUE,
-    description VARCHAR(250) NOT NULL UNIQUE
-);gi
+    description TEXT
+);
+
+CREATE TABLE vehicule (
+    id_vehicule INT PRIMARY KEY AUTO_INCREMENT,
+    modele VARCHAR(250) NOT NULL,
+    marque VARCHAR(250) NOT NULL,
+    prix_par_jour float NOT NULL,
+    disponibilite BOOLEAN DEFAULT TRUE,
+    categorie_id INT,
+    imageUrl VARCHAR(255),
+    description TEXT,
+    km float NOT null,
+    consom varchar(50) NOT null,
+    annee int NOT null,
+    place int NOT null,
+    FOREIGN KEY (categorie_id) REFERENCES categorie(id) ON DELETE SET NULL
+);
